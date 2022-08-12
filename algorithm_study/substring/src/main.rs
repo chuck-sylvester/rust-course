091c8cfbc1540e477b43a81ad01a743388e02dca
Chuck Sylvester <chuck.sylvester@outlook.com>
2022-08-12 08:11:56 -0400

    Learning Rust...

---
+++ algorithm_study/substring/src/main.rs
@@ -0,0 +1,19 @@
+// Find Substring algorithm
+// Determine whether or not string b can be found in string a
+
+// First attempt will be brute force
+//   create a and b string variables
+//   set values for a and b, e.g. "apple" and "ape"
+//   maybe get the string length of both and use to iterate
+// 
+
+
+fn main() {
+    println!("\nFind substring algorithm\n");
+
+    let a = "apple";
+    let b = "ape";
+
+    println!("The values of a and b are: {} and {}\n", a, b);
+
+}
