9235932ace710fecc09836dec751c3f669529abb
Chuck Sylvester <chuck.sylvester@outlook.com>
2022-08-30 21:28:34 -0400

    Working on LGR

---
+++ lgr/ch05/struct1/src/main.rs
@@ -0,0 +1,20 @@
+// The Rust Programming Language Book
+// Chapter 5 - Using Structs
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email
+}
